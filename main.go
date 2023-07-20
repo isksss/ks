3fd54e4a1c3f3835c4cd763c3aed61ed6ab9234d
isksss <104404522+isksss@users.noreply.github.com>
2023-07-20 11:12:40 +0900

    add color - red - blue - green - magenta - yellow - cyan

--- main.go
+++ main.go
@@ -1,23 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+
+	"github.com/fatih/color"
+)
 
 const (
 	ks = `
 	    ■■                  
 	    ■■        ■■■■■■■■  
 	■■■■■■■■■■           ■  
-	    ■    ■          ■
-	    ■    ■          ■
-	    ■    ■         ■
-	   ■■    ■        ■■
-	   ■     ■       ■■■■
-	  ■■     ■      ■■  ■■
-	 ■■     ■■    ■■■    ■■
-	 ■    ■■■    ■■       ■
+	    ■    ■          ■   
+	    ■    ■          ■   
+	    ■    ■         ■    
+	   ■■    ■        ■■    
+	   ■     ■       ■■■■   
+	  ■■     ■      ■■  ■■  
+	 ■■     ■■    ■■■    ■■ 
+	 ■    ■■■    ■■       ■ 
 `
 )
 
+var (
+	red     = flag.Bool("red", false, "color: red")
+	blue    = flag.Bool("blue", false, "color: blue")
+	green   = flag.Bool("green", false, "color: green")
+	magenta = flag.Bool("magenta", false, "color: magenta")
+	yellow  = flag.Bool("yellow", false, "color: yellow")
+	cyan    = flag.Bool("cyan", false, "color: cyan")
+)
+
+func init() {
+	flag.Parse()
+}
+
 func main() {
-	fmt.Println(ks)
+	var co *color.Color
+	switch {
+	case *red:
+		co = color.New(color.FgHiRed)
+	case *blue:
+		co = color.New(color.FgHiBlue)
+	case *green:
+		co = color.New(color.FgHiGreen)
+	case *magenta:
+		co = color.New(color.FgHiMagenta)
+	case *yellow:
+		co = color.New(color.FgHiYellow)
+	case *cyan:
+		co = color.New(color.FgHiCyan)
+	default:
+		co = color.New()
+	}
+	co.Add(color.Bold)
+
+	co.Println(ks)
 }
